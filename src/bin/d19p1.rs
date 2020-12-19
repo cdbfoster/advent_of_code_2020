694100bd61c30eb7c63db4bc3767431a1558fb7c
Chris Foster <cdbfoster@gmail.com>
2020-12-19 13:56:28 -0500

    Day 19.

---
+++ src/bin/d19p1.rs
@@ -0,0 +1,75 @@
+extern crate regex;
+
+use std::collections::HashMap;
+use std::env;
+use std::fs;
+
+use regex::Regex;
+
+enum Rule<'a> {
+    Value(&'a str),
+    Composition([Option<usize>; 2], [Option<usize>; 2]),
+}
+
+impl<'a> Rule<'a> {
+    fn compose(&self, rules: &HashMap<usize, Rule>) -> String {
+        match self {
+            Rule::Value(c) => c.to_string(),
+            Rule::Composition(a, b) => {
+                let rule_a: String = a
+                    .iter()
+                    .filter(|c| c.is_some())
+                    .map(|c| rules[&c.unwrap()].compose(rules))
+                    .collect::<Vec<_>>()
+                    .iter()
+                    .flat_map(|s| s.chars())
+                    .collect();
+                let rule_b: String = b
+                    .iter()
+                    .filter(|c| c.is_some())
+                    .map(|c| rules[&c.unwrap()].compose(rules))
+                    .collect::<Vec<_>>()
+                    .iter()
+                    .flat_map(|s| s.chars())
+                    .collect();
+                if rule_b.is_empty() {
+                    rule_a
+                } else {
+                    format!("(?:{}|{})", rule_a, rule_b)
+                }
+            }
+        }
+    }
+}
+
+fn main() {
+    let input = fs::read_to_string(env::args().nth(1).unwrap()).unwrap();
+    let sections = input.split("\n\n").collect::<Vec<_>>();
+
+    let rule = Regex::new(r#"^(\d+): "?(\d+|[ab])"?(?: (\d+))?(?: \| (\d+)(?: (\d+))?)?$"#).unwrap();
+
+    let rules = sections[0].lines().map(|l| {
+        let c = rule.captures(l).unwrap();
+        (
+            c.get(1).unwrap().as_str().parse::<usize>().unwrap(),
+            match c.get(2).unwrap().as_str() {
+                "a" | "b" => Rule::Value(c.get(2).unwrap().as_str()),
+                _ => Rule::Composition(
+                    [
+                        c.get(2).map(|c| c.as_str().parse().ok()).flatten(),
+                        c.get(3).map(|c| c.as_str().parse().ok()).flatten(),
+                    ],
+                    [
+                        c.get(4).map(|c| c.as_str().parse().ok()).flatten(),
+                        c.get(5).map(|c| c.as_str().parse().ok()).flatten(),
+                    ],
+                ),
+            },
+        )
+    })
+    .collect::<HashMap<_, _>>();
+
+    let rg = Regex::new(&format!("^{}$", rules[&0].compose(&rules))).unwrap();
+
+    println!("Matching strings: {}", sections[1].lines().filter(|l| rg.is_match(l)).count());
+}
